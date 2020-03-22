24b0979e04dbf9b8391a4c3eba9a6fd44707a304
Stephen Martin <sudormminusrf@gmail.com>
2020-03-22 09:22:31 +0000

    Added https for prod / staging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (in_array($this->app->environment(), ['production','staging'])) {
+            URL::forceScheme('https');
+        }
     }
 }
