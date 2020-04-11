ed960ab7e7bdae9e3eef83e497cee10f8f5ec764
Stephen Martin <sudormminusrf@gmail.com>
2020-04-11 19:37:23 +0100

    Cleaning up products controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,13 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Transformers\ProductTransformer;
-use App\Actions\vendors\CreateProductAction;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use League\Fractal\Pagination\IlluminatePaginatorAdapter;
-use Symfony\Component\HttpFoundation\Response;
-use App\Models\Vendor;
 
 class ProductsController extends Controller
 {
