be6b3f17c8cd87a894fac13f1d1ff0adbbd289e4
Stephen Martin <sudormminusrf@gmail.com>
2020-04-14 09:08:27 +0100

    Adding description to product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,6 +9,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(3, true),
+        'description' => $faker->sentence(3, true),
         'vendor_id' => factory('App\Models\Vendor'),
         'price' => random_int(100,10000),
     ];
